1ccd2bb8fc7a24dcc57839bd18381768ee8e5230
Heidy Hazem Mohammed <32885335+heidyhazem@users.noreply.github.com>
2019-11-28 09:46:39 +0200

    lambda doc

--- src/main/java/PoissonGenerator.java
+++ src/main/java/PoissonGenerator.java
@@ -1,5 +1,10 @@
 public class PoissonGenerator {
 
+    /**
+     * This method is used to generate a rondom number from poission distribution accepting one parameters
+     * @param lambda This is the first parameter to getPoisson method. It is a double lambda paramter
+     * @return int This returns random number with respect to poisson distribution.
+     */
     public static int getPoisson(double lambda) {
         double L = Math.exp(-lambda);
         double p = 1.0;
