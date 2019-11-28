22c40fc52b04f289f0156370a9e59bdb9062343b
Heidy Hazem Mohammed <32885335+heidyhazem@users.noreply.github.com>
2019-11-28 09:40:25 +0200

    modify docs

--- src/main/java/Evaluation.java
+++ src/main/java/Evaluation.java
@@ -6,7 +6,15 @@ public class Evaluation {
 
     //*** Evaluation class includes the evaluation methods for online recommender systems**//
 
-    //precisionOnline caluclates online precision = |E ∩ L| / k
+
+
+    /**
+     * This method is used to calculate the precision considering prequential evaluation accepting two parameters
+     * precisionOnline calculates online precision = |E ∩ L| / k.
+     * @param consumedItem This is the first parameter to precisionOnline method. It is a string item in the current interaction
+     * @param recommendedList  This is the second parameter to precisionOnline method. It is an Arraylist of the recommended items
+     * @return Float This returns online precision for the current interaction.
+     */
     Float precisionOnline(String consumedItem, ArrayList<String> recommendedList){
         Integer k = recommendedList.size();
         if (recommendedList.contains(consumedItem)){
@@ -17,7 +25,14 @@ public class Evaluation {
         }
     }
 
-    //recallOnline calculates online Recall = |E ∩ L| / |E|
+
+    /**
+     * This method is used to calculate the recall considering prequential evaluation accepting two parameters
+     * recallOnline calculates online Recall = |E ∩ L| / |E|.
+     * @param consumedItem This is the first parameter to recallOnline method. It is a string item in the current interaction
+     * @param recommendedList  This is the second parameter to recallOnline method. It is an Arraylist of the recommended items
+     * @return Integer This returns online recall for the current interaction.
+     */
     Integer recallOnline(String consumedItem, ArrayList<String> recommendedList){
       if (recommendedList.contains(consumedItem)){
           //1/1
@@ -30,7 +45,13 @@ public class Evaluation {
     }
 
     //because of online case it is identical to recall
-    //ClickThrough = 1 if E∩L ̸= 0/  and zero otherwise;
+    /**
+     * This method is used to calculate the clickThrough considering prequential evaluation accepting two parameters
+     * ClickThrough = 1 if E∩L ̸= 0/  and zero otherwise
+     * @param consumedItem This is the first parameter to clickThrough method. It is a string item in the current interaction
+     * @param recommendedList  This is the second parameter to clickThrough method. It is an Arraylist of the recommended items
+     * @return Integer This returns online clickThrough for the current interaction.
+     */
     Integer clickThrough(String consumedItem, ArrayList<String> recommendedList){
         if (recommendedList.contains(consumedItem)){
             return 1;
@@ -40,7 +61,14 @@ public class Evaluation {
         }
     }
 
-    //reciprocalRank is considering the position of the relevant item
+
+    /**
+     * This method is used to calculate the reciprocalRank considering prequential evaluation accepting two parameters
+     * reciprocalRank is considering the position of the relevant item
+     * @param consumedItem This is the first parameter to reciprocalRank method. It is a string item in the current interaction
+     * @param recommendedList  This is the second parameter to reciprocalRank method. It is an Arraylist of the recommended items
+     * @return Float This returns online reciprocalRank for the current interaction.
+     */
     Float reciprocalRank(String consumedItem, ArrayList<String> recommendedList){
         if (recommendedList.contains(consumedItem)){
             Integer rank = recommendedList.indexOf(consumedItem);
@@ -51,7 +79,14 @@ public class Evaluation {
         }
     }
 
-    //Discounted cumulative gain DCG@K =  ∑ rel(ik)/log (1+k)
+
+    /**
+     * This method is used to calculate the DisCumulativeGain considering prequential evaluation accepting two parameters
+     * Discounted cumulative gain DCG@K =  ∑ rel(ik)/log (1+k)
+     * @param consumedItem This is the first parameter to DisCumulativeGain method. It is a string item in the current interaction
+     * @param recommendedList  This is the second parameter to DisCumulativeGain method. It is an Arraylist of the recommended items
+     * @return Double This returns online DisCumulativeGain for the current interaction.
+     */
     Double DisCumulativeGain(String consumedItem, ArrayList<String> recommendedList){
 
         if (recommendedList.contains(consumedItem)){
